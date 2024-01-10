c2ade0250d71db4c823aaa939bbb63c506afee93
Andrey <itsfera85@gmail.com>
2024-01-10 14:53:04 +0500

    Add Tak1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] arr = new int[5];
+arr[0] = 1;
+arr[1] = 5;
+arr[2] = 7;
+arr[3] = 5;
+arr[4] = 2;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int [5] {1,5,3,2,7};
+
+int[] arr3= {3,2,1,5,7};
+
+Console.WriteLine(arr2[0]);
+Console.WriteLine(arr2[1]);
+Console.WriteLine(arr2[2]);
+Console.WriteLine(arr2[3]);
+Console.WriteLine(arr2[4]);
+
+Console.WriteLine(arr3[0]);
+Console.WriteLine(arr3[1]);
+Console.WriteLine(arr3[2]);
+Console.WriteLine(arr3[3]);
+Console.WriteLine(arr3[4]);
