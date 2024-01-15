d9f4ed296aec36665472411cd65b9336bcd96a5b
Andrey <itsfera85@gmail.com>
2024-01-15 17:43:01 +0500

    Add Task1

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿double CalculateFormula (int a, int b, int c, int d)
+{
+    int numenator = a*b;
+    int denumenator = c+d;
+    double result = numenator/denumenator;
+    return result;
+}
+Console.WriteLine(CalculateFormula(2,5,1,1));
